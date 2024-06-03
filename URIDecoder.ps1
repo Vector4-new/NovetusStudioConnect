673a295e6dd3c707751573ce0900792a152fea81
Vector4.new <68828797+Vector4-new@users.noreply.github.com>
2024-06-03 22:43:42 +0100

    fix decoding

--- URIDecoder.ps1
+++ URIDecoder.ps1
@@ -7,13 +7,30 @@ param(
 
 $ErrorActionPreference = "Stop"
 
+Add-Type -AssemblyName System.Security
+Add-Type -AssemblyName System
+
+function Decrypt {
+    param ( $str )
+
+    $key = [byte] 1, 2, 3, 4, 5, 6, 7, 8
+    $iv = [byte] 1, 2, 3, 4, 5, 6, 7, 8
+
+    $algorithm = [System.Security.Cryptography.DES]::Create()
+    $transform = $algorithm.CreateDecryptor($key, $iv)
+    $inputBuffer = [System.Convert]::FromBase64String($str)
+    $outputBuffer = $transform.TransformFinalBlock($inputBuffer, 0, $inputBuffer.Length)
+
+    return [System.Text.Encoding]::Unicode.GetString($outputBuffer)
+}
+
 $URI = $URI -replace "^.*://"
 
 $arr = ([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($URI))).Split("|")
 
-$ip = ([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($arr[0])))
-$port = ([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($arr[1])))
-$version = ([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($arr[2])))
+$ip = Decrypt($arr[0])
+$port = Decrypt($arr[1])
+$version = Decrypt($arr[2])
 
 "Server IP: $ip"
 "Server Port: $port"
